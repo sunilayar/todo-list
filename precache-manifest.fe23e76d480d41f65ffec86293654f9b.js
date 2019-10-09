05260b7007d5fd2e5abd8f120874e5d950b4cd11
sunil ayar <sunilayarhassan@gmail.com>
2019-10-09 18:53:04 +0530

    Updates

---
+++ precache-manifest.fe23e76d480d41f65ffec86293654f9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b03fefd9d52e794daed2446e168907b",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "2189c5883891577ef3c6",
+    "url": "/todo-list/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "aef6d97fa4ddee6d4357",
+    "url": "/todo-list/static/css/main.e46a36f9.chunk.css"
+  },
+  {
+    "revision": "2189c5883891577ef3c6",
+    "url": "/todo-list/static/js/2.e0893b50.chunk.js"
+  },
+  {
+    "revision": "aef6d97fa4ddee6d4357",
+    "url": "/todo-list/static/js/main.f2ef89c7.chunk.js"
+  },
+  {
+    "revision": "d5299376858e34d7f886",
+    "url": "/todo-list/static/js/runtime-main.95cb5688.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todo-list/static/media/logo.25bf045c.svg"
+  }
+]);
